4df924aa7ebfa1095c07af4aca07fd6f8e6dc6ca
sujitx-vs <sujith.vs.cmfeb26@gmail.com>
2026-07-17 02:05:58 +0530

    Schema - DataFrame - Spark

---
+++ test37.py
@@ -0,0 +1,23 @@
+from pyspark.sql import SparkSession
+
+# Create Spark Session
+spark = SparkSession.builder.appName("Schema Demo").getOrCreate()
+
+# Sample Data
+data = [
+    ("Rahul", 25, 50000),
+    ("Anu", 22, 60000),
+    ("John", 30, 55000),
+    ("Riya", 28, 70000)
+]
+
+# Create DataFrame
+df = spark.createDataFrame(
+    data,
+    ["Name", "Age", "Salary"]
+)
+
+print("Schema:")
+df.printSchema()
+
+spark.stop()
